8d9950700b163b9ae8d1f2fc89b7fe2f1673014c
Javi-cba <cordobajava@gmail.com>
2024-10-05 14:10:45 -0300

    JCJCC223231javiJC

--- src/models/product.js
+++ src/models/product.js
@@ -12,8 +12,24 @@ const productSchema = new mongoose.Schema({
   category: { type: String, required: true },
   thumbnail: { type: String, required: true },
   images: [{ type: String }], // Array de URLs de imágenes
+  weight: { type: Number, required: false }, // Peso opcional
+  dimensions: {
+    width: { type: Number, required: false }, // Ancho opcional
+    height: { type: Number, required: false }, // Alto opcional
+    depth: { type: Number, required: false }, // Profundidad opcional
+  },
+  reviews: [
+    {
+      rating: { type: Number, required: true },
+      comment: { type: String, required: true },
+      date: { type: Date, required: true },
+      reviewerName: { type: String, required: true },
+      reviewerEmail: { type: String, required: true },
+    },
+  ],
 });
 
+// Crear el modelo de producto
 const productModel = mongoose.model('Product', productSchema);
 
 module.exports = productModel; // Exportar con CommonJS
